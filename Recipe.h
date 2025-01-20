cca2ff4bac32b07f313371fb55a8ec183e09a093
DHRUV VERMA <azaditya129@gmail.com>
2025-01-20 11:01:18 +0530

    Update Recipe.h

--- Recipe.h
+++ Recipe.h
@@ -1,26 +1,18 @@
-#ifndef RECIPE_MANAGER_H
-#define RECIPE_MANAGER_H
+#ifndef RECIPE_H
+#define RECIPE_H
 
-#include "Recipe.h"
-#include <vector>
 #include <string>
 
-class RecipeManager {
+class Recipe {
 public:
-    void addRecipe(const Recipe& recipe);
-    void editRecipe(const std::string& name, const Recipe& newRecipe);
-    void deleteRecipe(const std::string& name);
-    void searchByIngredient(const std::string& ingredient) const;
-    void searchByName(const std::string& name) const;
-    void listRecipesByCategory(const std::string& category) const;
-    void searchByMultipleIngredients(const std::vector<std::string>& ingredients) const;
-    void filterByRating(int rating) const;
-    void saveToFile(const std::string& filename) const;
-    void loadFromFile(const std::string& filename);
+    // Add your members and methods here
+    std::string name;
+    std::string ingredient;
+    int rating;
 
-private:
-    std::vector<Recipe> recipes;
+    // Constructor, methods, etc.
+    Recipe(const std::string& name, const std::string& ingredient, int rating)
+        : name(name), ingredient(ingredient), rating(rating) {}
 };
 
-#endif // RECIPE_MANAGER_H
-
+#endif // RECIPE_H
