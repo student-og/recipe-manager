4de4e1cf6b3de1e162ba60203152b9301e883d55
DHRUV VERMA <azaditya129@gmail.com>
2025-01-20 09:50:01 +0530

    Update RecipeManager.cpp

--- RecipeManager.cpp
+++ RecipeManager.cpp
@@ -1,19 +1,14 @@
+#include "RecipeManager.h"
+#include <fstream>
+#include <iostream>
+
 void RecipeManager::saveToFile(const std::string& filename) const {
     std::ofstream file(filename);
     if (file.is_open()) {
         for (const auto& recipe : recipes) {
-            file << recipe.getName() << "\n";
-            file << recipe.getCategory() << "\n";
-            for (const auto& ingredient : recipe.getIngredients()) {
-                file << ingredient << "\n";
-            }
-            file << recipe.getRating() << "\n";
-            for (const auto& comment : recipe.getComments()) {
-                file << comment << "\n";
-            }
-            file << "---\n";
+            // Assuming Recipe has a method to serialize to a string
+            file << recipe.serialize() << "\n";
         }
-        file.close();
     } else {
         std::cerr << "Unable to open file for writing.\n";
     }
@@ -25,22 +20,20 @@ void RecipeManager::loadFromFile(const std::string& filename) {
         recipes.clear();
         std::string name, category, ingredient, comment;
         std::vector<std::string> ingredients, comments;
-        int rating;
+        int rating; 
+
         while (std::getline(file, name)) {
             std::getline(file, category);
             ingredients.clear();
             while (std::getline(file, ingredient) && ingredient != "---") {
                 ingredients.push_back(ingredient);
             }
-            file >> rating;
-            file.ignore();
             comments.clear();
             while (std::getline(file, comment) && comment != "---") {
                 comments.push_back(comment);
             }
             recipes.push_back(Recipe(name, category, ingredients, rating, comments));
         }
-        file.close();
     } else {
         std::cerr << "Unable to open file for reading.\n";
     }
