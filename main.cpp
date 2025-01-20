f86bf8ce4481793432b40432443ac4d347d2a11f
DHRUV VERMA <azaditya129@gmail.com>
2025-01-20 10:54:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "MainWindow.h"
 #include <QApplication>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow mainWindow;
     mainWindow.show();
